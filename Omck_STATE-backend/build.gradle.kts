f7318229a0fab2b65f0666a7efa0b9137d8ca951
Artem Gruzdev <myposhta.ruzdev@gmail.com>
2023-04-24 14:18:14 +0600

    [+] add dep

--- Omck_STATE-backend/build.gradle.kts
+++ Omck_STATE-backend/build.gradle.kts
@@ -11,6 +11,13 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.poi:poi-ooxml:5.2.2")
+    implementation("org.apache.poi:poi:3.7")
+    implementation ("org.jetbrains.exposed:exposed-core:0.41.1")
+    val ktor = "2.2.4"
+    implementation("io.ktor:ktor-server-cio:$ktor")
+    implementation("io.ktor:ktor-server-cors:$ktor")
+
     testImplementation(kotlin("test"))
 }
 
